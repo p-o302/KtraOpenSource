e6db3a00ff4a6c23be9e38835f69b2345a985a21
Thao Nguyen <55941949+p-o302@users.noreply.github.com>
2021-08-11 14:44:24 +0700

    kt

---
+++ trangchu2.php
@@ -0,0 +1,159 @@
+<?php if (session_id() == '' || !isset($_SESSION)) {
+  session_start();
+} ?>
+<!DOCTYPE html>
+<html>
+<head>
+<!--     <link rel="stylesheet" href="css/qlbh.css" /> -->
+    <title>ADMIN</title>
+</head>
+<body>
+  <div>
+    <style type="text/css">
+      * {
+  margin: 0;
+  padding: 0;
+}
+.button {
+  -moz-box-shadow:inset 0px 1px 0px 0px #ffffff;
+  -webkit-box-shadow:inset 0px 1px 0px 0px #ffffff;
+  box-shadow:inset 0px 1px 0px 0px #ffffff;
+  background:-webkit-gradient( linear, left top, left bottom, color-stop(0.05, #ededed), color-stop(1, #dfdfdf) );
+  background:-moz-linear-gradient( center top, #ededed 5%, #dfdfdf 100% );
+  filter:progid:DXImageTransform.Microsoft.gradient(startColorstr='#ededed', endColorstr='#dfdfdf');
+  background-color:#ededed;
+  -webkit-border-top-left-radius:6px;
+  -moz-border-radius-topleft:6px;
+  border-top-left-radius:6px;
+  -webkit-border-top-right-radius:6px;
+  -moz-border-radius-topright:6px;
+  border-top-right-radius:6px;
+  -webkit-border-bottom-right-radius:6px;
+  -moz-border-radius-bottomright:6px;
+  border-bottom-right-radius:6px;
+  -webkit-border-bottom-left-radius:6px;
+  -moz-border-radius-bottomleft:6px;
+  border-bottom-left-radius:6px;
+  text-indent:0;
+  border:1px solid #dcdcdc;
+  display:inline-block;
+  color:#777777;
+  font-family:Times New Roman;
+  font-size:15px;
+  font-weight:normal;
+  font-style:normal;
+  height:25px;
+  line-height:25px;
+  width:100px;
+  text-decoration:none;
+  text-align:center;
+  text-shadow:1px 1px 0px #ffffff;
+}
+.button:hover {
+  background:-webkit-gradient( linear, left top, left bottom, color-stop(0.05, #dfdfdf), color-stop(1, #ededed) );
+  background:-moz-linear-gradient( center top, #dfdfdf 5%, #ededed 100% );
+  filter:progid:DXImageTransform.Microsoft.gradient(startColorstr='#dfdfdf', endColorstr='#ededed');
+  background-color:#dfdfdf;
+}.button:active {
+  position:relative;
+  top:1px;
+}
+/* This button was generated using http://tools.ngoisaoso.vn/css3button/ */
+
+
+#menu {
+        font-size: 15px;
+    color: white;
+    padding: 5px 5px 0 5px;
+    background-color: #FF6600;
+    height: 30px;
+    top: 89px;
+    right: 0px;
+   text-align: right;
+    border-bottom-left-radius: 5px;
+    border-bottom-right-radius: 5px;
+}
+#menu a{
+  color: white;
+  padding: 5px;
+  text-decoration: none;
+  text-align: center;
+  
+  right: 5px;
+
+}
+.h1{
+  font-family: arial;
+  width: 300px;
+  height: 50px;
+  padding-left: 10px;
+  font-size: 20px;
+  font-weight: bold;
+  text-align: justify;
+}
+.h2{
+    font-family: arial;
+  width: 700px;
+  height: 150px;
+  padding-left: 10px;
+  font-size: 18px;
+  text-align: justify;
+  padding-top: 5px;
+}
+#news-block-title {
+    font-size: 18px;
+        border-bottom: 1px solid #cd2122;
+        position: relative;
+        left: 300px;
+        font-size: 30px;
+        top: 50px;
+
+}
+p{
+  padding-top: 10px;
+  font-size: 16px;
+  font-weight: normal;
+}
+.a3{
+  font-size: 20px;
+  text-decoration: none;
+  font-style: bold;
+  color: black;
+  font-size: 20px;
+}
+.a3:hover{
+  color: #CD2122;
+}
+.tr{
+      margin: 0 0 30px;
+    overflow: hidden;
+    list-style-type: none;
+}
+    </style>
+    <div align="center">
+        <a href="">
+            <img src="img/logo_truong.png">
+        </a>
+    </div>
+<div id="menu">
+    
+    <a href="trangchu2.php">Trang Chủ</a>
+    |
+    <a href="view/quanlysinhvien.php">Quản lý sinh viên</a>
+    |
+     <a href="view/quanlymonhoc.php">Quản lý môn học</a>
+    |
+    
+      Bạn là ADMIN -
+    <a href="login.php">thoát</a>
+    
+
+</div>
+</div>
+<div >
+<span style="z-index: -1; " id="news-block-title">TIN TỨC SỰ KIỆN</span>
+
+
+</div>
+</body>
+</html>
